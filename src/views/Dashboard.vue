d7bdd08597969d3e2b1f7e726aaf8549d817de14
Malte Schwitters <malte.schwitters@outlook.de>
2019-01-31 23:55:33 +0100

    added details page

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,38 +1,28 @@
 <template>
     <div class="flex-col p-4">
         <div class="h-full flex-grow flex-row flex-items-stretch">
-            <div v-for="i in 4" class="w-64 flex-grow card m-1 card-hover flex-col">
+            <router-link to="details" v-for="i in 4" class="w-64 flex-grow card m-1 card-hover flex-col">
                 <img class="dashboard-image-bw" src="../../public/games/csgo/csgo_square_bw.jpg"/>
                 <img class="dashboard-image-color" src="../../public/games/csgo/csgo_square.jpg"/>
                 <div class="p-1">
                     <div class="flex-row flex-items-center">
-                        <!--<img class="icon" src="https://img.icons8.com/ios-glyphs/50/ffffff/controller.png">-->
                         <h3>Counter Strike Go</h3>
                     </div>
-                    <!--<div class="flex-row flex-items-center">-->
-                    <!--<img class="icon" src="https://img.icons8.com/ios-glyphs/50/ffffff/trophy.png">-->
-                    <!--<h3>Turnier</h3>-->
-                    <!--</div>-->
-                    <!--<div class="flex-row flex-items-center">-->
-                    <!--<img class="icon" src="https://img.icons8.com/ios-glyphs/50/ffffff/place-marker.png">-->
-                    <!--<h3>Turnier Rechner</h3>-->
-                    <!--</div>-->
                     <div class="flex-row flex-items-center">
-                        <!--<img class="icon"-->
-                        <!--src="https://img.icons8.com/ios-glyphs/50/ffffff/conference-background-selected.png">-->
                         <h3>{{ i }} / 10 Teilnehmer</h3>
                     </div>
                     <div class="flex-row flex-items-center">
-                        <!--<img class="icon" src="https://img.icons8.com/ios-glyphs/50/ffffff/time-span.png">-->
                         <h3>in {{ i }} Minute(n)</h3>
                     </div>
                 </div>
-            </div>
-            <div class="w-128 flex-grow flex-col">
-                <div v-for="i in 7" class="card card-hover dashboard-list-item">
-                    <img src="../../public/games/csgo/csgo_square_bw.jpg"/>
-                    <p class="flex-grow">Counter Strike Go</p>
-                    <p>in {{ i }} Minuten</p>
+            </router-link>
+            <div class="w-128 p-1 flex-grow">
+                <div class="card flex-col h-full">
+                    <div v-for="i in 7" class="card-hover dashboard-list-item">
+                        <img src="../../public/games/csgo/csgo_square_bw.jpg"/>
+                        <p class="flex-grow">Counter Strike Go</p>
+                        <p>in {{ i }} Minuten</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -64,21 +54,22 @@
 
     .dashboard-list-item {
         height: 5rem;
-        margin: 1rem 1rem;
+        margin: 0.5rem 0;
         display: flex;
         align-items: center;
         flex-grow: 1;
     }
 
     .dashboard-list-item > img {
+        margin-left: 1rem;
         align-self: stretch;
-        border-top-left-radius: 0.3rem;
-        border-bottom-left-radius: 0.3rem;
+        /*border-top-left-radius: 0.3rem;*/
+        /*border-bottom-left-radius: 0.3rem;*/
         height: 100%;
     }
 
     .dashboard-list-item > p {
-        padding: 0 0.5rem;
+        margin: 0 0.5rem;
     }
 
     .timer {
--- src/views/Tournament.vue
+++ src/views/Tournament.vue
@@ -1,9 +1,80 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="flex-col p-4">
+        <div class="m-1 flex-row flex-items-center">
+            <h1 class="flex-grow">Counter Strike Global Offensive</h1>
+            <h3>startet in:</h3>
+            <h3>00:15</h3>
+            <button>
+                Teilnehmen
+            </button>
+            <button>
+                Bearbeiten
+            </button>
+        </div>
+        <div class="h-full flex-grow flex-row flex-items-stretch">
+            <div class="w-64 m-1 card flex-grow">
+                <img class="game-image" src="../../public/games/csgo/csgo_square_bw.jpg"/>
+            </div>
+            <div class="w-64 m-1 p-1 card flex-grow">
+                <h2>Details</h2>
+                <table class="w-full">
+                    <tr>
+                        <td><b>Spiel</b></td>
+                        <td>Counter Strike: Global Offensive</td>
+                    </tr>
+                    <tr>
+                        <td><b>Modus</b></td>
+                        <td>Ausscheidungs - Turnier</td>
+                    </tr>
+                    <tr>
+                        <td><b>Ort</b></td>
+                        <td>Turnierrechner</td>
+                    </tr>
+                    <tr>
+                        <td><b>Erstellt von</b></td>
+                        <td>Spieler 1</td>
+                    </tr>
+                    <tr>
+                        <td><b>Startet</b></td>
+                        <td>12:00</td>
+                    </tr>
+                </table>
+                <p>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                    labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+                    et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                    labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+                    et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </p>
+            </div>
+            <div class="w-64 m-1 p-1 card flex-grow">
+                <h2>Teilnehmer</h2>
+                <div v-for="i in 4">
+                    <div class="flex-row">
+                        <p class="flex-grow">Team {{ i }}</p>
+                        <p>{{ i }} / 4 Spieler</p>
+                    </div>
+                    <div class="flex-row">
+                        <div v-for="j in i">
+                            <img src="https://img.icons8.com/ios/50/ffffff/name-filled.png">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="h-32"></div>
     </div>
 </template>
+
+<style>
+    .game-image {
+        width: 100%;
+        height: 100%;
+        object-fit: fill;
+        border-radius: inherit;
+    }
+</style>
 
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator';
